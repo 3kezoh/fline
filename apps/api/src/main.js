0bf691b0ea17751ecbd6f0142d44878f8e92761b
3kezoh <3kezoh@gmail.com>
2022-07-17 00:44:26 +0200

    feat(api): :card_file_box: connect to database

--- apps/api/src/main.js
+++ apps/api/src/main.js
@@ -4,6 +4,29 @@
  */
 
 import * as express from 'express';
+import { sequelize } from '@fline/sequelize';
+
+// TODO: connection should be done in a separate file
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection to postgresql successful');
+
+    // TODO: sync should done be in a separate file
+    if (process.env.NODE_ENV === 'development') {
+      sequelize
+        .sync({ force: true })
+        .then(() => {
+          console.log('Database synced');
+        })
+        .catch((err) => {
+          console.error('Error syncing database', err);
+        });
+    }
+  })
+  .catch((err) => {
+    console.error('Error connecting to postgresql', err);
+  });
 
 const app = express();
 

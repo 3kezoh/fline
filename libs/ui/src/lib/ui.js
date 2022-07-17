c9e81eeb9ade1997adc02938bc54bed683d2fb4e
3kezoh <3kezoh@gmail.com>
2022-07-17 19:45:34 +0200

    feat(ui): :package: add ui lib

---
+++ libs/ui/src/lib/ui.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+const StyledUi = styled.div`
+  color: pink;
+`;
+export function Ui(props) {
+  return (
+    <StyledUi>
+      <h1>Welcome to Ui!</h1>
+    </StyledUi>
+  );
+}
+export default Ui;

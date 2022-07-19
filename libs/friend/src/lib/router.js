79c9d13ce500d60a88feb9858fa25c71f32f3692
louispelarrey <louispelarrey@gmail.com>
2022-07-20 01:47:24 +0200

    feat(api): friends now got every main features

--- libs/friend/src/lib/router.js
+++ libs/friend/src/lib/router.js
@@ -1,7 +1,28 @@
 import { authenticate } from '@fline/security';
 import { Router } from 'express';
-import { getfriend } from './controller';
+import {
+  addFriend,
+  blockFriend,
+  getFriend,
+  acceptPendingFriend,
+  rejectFriend,
+  removeFriend,
+  unblockFriend,
+  getBlockedFriends,
+  getPendingFriends,
+  getRealFriends,
+} from './controller';
 
 export const friendRouter = Router();
 
-friendRouter.get('/getfriend', authenticate, getfriend);
+friendRouter
+  .get('/friend', authenticate, getRealFriends)
+  .post('/friend', authenticate, addFriend)
+  .delete('/friend', authenticate, removeFriend)
+  .post('/friend/block', authenticate, blockFriend)
+  .post('/friend/unblock', authenticate, unblockFriend)
+  .post('/friend/accept', authenticate, acceptPendingFriend)
+  .post('/friend/reject', authenticate, rejectFriend)
+  .get('/friend/blocked', authenticate, getBlockedFriends)
+  .get('/friend/pending', authenticate, getPendingFriends)
+  .get('/friend/all', authenticate, getFriend);
